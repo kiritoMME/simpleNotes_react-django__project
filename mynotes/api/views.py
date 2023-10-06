34fb5cf0d8d88ecb4cd0c936fd6d6c20faaf739b
kiritoMME <77681921+kiritoMME@users.noreply.github.com>
2023-10-06 12:40:17 +0300

    Full BackEnd Project & Base of react project

---
+++ mynotes/api/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {
+            "name" : "mohhmed",
+            "id" : 1
+        }
+    ]
+
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(requset, pk):
+    data = requset.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id = pk)
+    note.delete()
+    return Response("Note was deleted!")
